95bf6c261cdf59a6b20a57b50c1d1fa652eda1b5
sangyog <sangyog.puri111@gmail.com>
2025-02-06 23:38:02 +0545

    env file fix for azure

--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prismaClient = new PrismaClient();
 
 async function connectDb() {
   try {
