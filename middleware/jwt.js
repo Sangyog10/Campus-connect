0ca49027207ed9e5fabd9b5be967256b992b119a
sangyog <sangyog.puri111@gmail.com>
2025-02-18 09:16:33 +0545

    Change JWT expiration to 1 year

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "365d",
+  });
   return token;
 };
 
@@ -10,7 +12,7 @@ const isTokenValid = async (token) => jwt.verify(token, process.env.JWT_SECRET);
 const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
   res.cookie("token", token, {
-    expires: new Date(Date.now() + 24 * 60 * 60 * 100),
+    expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
   });
